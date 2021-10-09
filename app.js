cf5a907f31e373eaf340dd74f9e6518d40bb8781
zach <ZCroninDev@users.noreply.github.com>
2021-10-09 18:42:03 -0400

    ui build WIP

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 // Init weather object
 const weather = new Weather('Boston', 'MA');
 
-weather.getWeather()
-.then(results => {
-  console.log(results);
-})
-.catch(err => console.log(err));
+// Get weather on DOM load
+document.addEventListener('DOMContentLoaded', getWeather)
 
+// weather.changeLocation('Miami', 'FL')
+
+function getWeather(){
+  weather.getWeather()
+  .then(results => {
+    console.log(results);
+    ui.paint(results);
+  })
+  .catch(err => console.log(err));
+}
